cc3535067c2753f84ac4e039ca38cca6aaa10600
j45 <janscheresmonesma@gmail.com>
2023-03-14 17:44:35 +0000

    Basic caching functionality

--- proxy.go
+++ proxy.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 
 	"github.com/miekg/dns"
 )
@@ -17,6 +18,14 @@ var upstreamIP net.IP = net.ParseIP("208.67.220.220")
 var upstreamPort string = "853"
 
 var localPort string = "5353" //853 for tls
+
+type cacheEntry struct {
+	q dns.Question
+	a dns.RR
+	t time.Time
+}
+
+var cacheValidTime time.Duration = 300 * time.Second
 
 func readPacket(conn net.Conn) ([]byte, []byte, error) {
 	buf := make([]byte, 2)
@@ -44,7 +53,7 @@ func readPacket(conn net.Conn) ([]byte, []byte, error) {
 	return raw, buf, nil
 }
 
-func handleConnection(localConn net.Conn, tlsConf *tls.Config) {
+func handleConnection(localConn net.Conn, tlsConf *tls.Config, c *[]cacheEntry) {
 	upstreamDialer := &tls.Dialer{
 		Config: tlsConf,
 	}
@@ -66,7 +75,38 @@ func handleConnection(localConn net.Conn, tlsConf *tls.Config) {
 			if err != nil {
 				log.Fatal(err)
 			}
-			log.Println(m.Question[0])
+
+			//check cache for the question
+			for i, entry := range *c {
+				if entry.q == m.Question[0] {
+
+					//check if cache entry is still valid
+					if time.Since(entry.t) < cacheValidTime {
+
+						response := new(dns.Msg)
+						response.SetReply(m)
+						response.Answer = append(response.Answer, entry.a)
+
+						responseRaw, err := response.Pack()
+						if err != nil {
+							return
+						}
+
+						//add 2 bytes length to the beginning of the packet
+						responseRaw = append([]byte{byte(len(responseRaw) >> 8), byte(len(responseRaw))}, responseRaw...)
+
+						_, err = localConn.Write(responseRaw)
+						if err != nil {
+							return
+						}
+
+						continue
+					} else {
+						//delete entry if it is not valid anymore
+						*c = append((*c)[:i], (*c)[i+1:]...)
+					}
+				}
+			}
 
 			_, err = upstreamConn.Write(raw)
 			if err != nil {
@@ -86,8 +126,15 @@ func handleConnection(localConn net.Conn, tlsConf *tls.Config) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println(m.Question[0])
-		log.Println(m.Answer[0])
+
+		for i, entry := range *c {
+			if entry.q == m.Question[0] {
+				*c = append((*c)[:i], (*c)[i+1:]...) //delete entry if it already exists
+			}
+		}
+
+		//add to cache
+		*c = append(*c, cacheEntry{m.Question[0], m.Answer[0], time.Now()})
 
 		_, err = localConn.Write(raw)
 		if err != nil {
@@ -97,8 +144,18 @@ func handleConnection(localConn net.Conn, tlsConf *tls.Config) {
 }
 
 func main() {
+	var c []cacheEntry
+
 	certPEMBlock, err := os.ReadFile("cert.pem")
+	if err != nil {
+		log.Fatal("error parsing public key, `cert.pem`", err)
+	}
+
 	keyPEMBlock, err := os.ReadFile("key.pem")
+	if err != nil {
+		log.Fatal("error parsing private key, `key.pem`", err)
+	}
+
 	cert, err := tls.X509KeyPair(certPEMBlock, keyPEMBlock)
 	if err != nil {
 		log.Fatal(err)
@@ -111,11 +168,23 @@ func main() {
 		log.Fatal(err)
 	}
 
+	go func() {
+		//clear outdated cache entries
+		for {
+			for i, entry := range c {
+				if time.Since(entry.t) > cacheValidTime {
+					c = append(c[:i], c[i+1:]...)
+				}
+			}
+			time.Sleep(cacheValidTime / 2)
+		}
+	}()
+
 	for {
 		localConn, err := local.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-		go handleConnection(localConn, tlsConf)
+		go handleConnection(localConn, tlsConf, &c)
 	}
 }
