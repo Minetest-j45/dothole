fd12e7c20241f3ed88c9cbe572d5c9f194778320
j45 <janscheresmonesma@gmail.com>
2023-03-15 16:53:26 +0000

    Blocklist flags

--- proxy.go
+++ proxy.go
@@ -34,8 +34,8 @@ type cache struct {
 
 var cacheValidTime time.Duration = 10 * time.Second //todo: change to around 300 seconds
 
-func loadList() map[string]string {
-	resp, err := http.Get("https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts")
+func loadList(url string) map[string]string {
+	resp, err := http.Get(url)
 	if err != nil {
 		return nil
 	}
@@ -54,7 +54,6 @@ func loadList() map[string]string {
 
 		// add to blocklist
 		blockList[parts[1]+"."] = parts[0]
-
 	}
 
 	return blockList
@@ -86,7 +85,7 @@ func readPacket(conn net.Conn) ([]byte, []byte, error) {
 	return raw, buf, nil
 }
 
-func handleConnection(localConn net.Conn, tlsConf *tls.Config, c *cache, blockList map[string]string) {
+func handleConnection(localConn net.Conn, tlsConf *tls.Config, c *cache, block bool, blockList map[string]string) {
 	upstreamDialer := &tls.Dialer{
 		Config: tlsConf,
 	}
@@ -114,7 +113,6 @@ func handleConnection(localConn net.Conn, tlsConf *tls.Config, c *cache, blockLi
 			c.Lock()
 			for i, entry := range c.entries {
 				if entry.q == m.Question[0] {
-
 					//check if cache entry is still valid
 					if time.Since(entry.t) < cacheValidTime {
 						dontQuery = true
@@ -140,40 +138,41 @@ func handleConnection(localConn net.Conn, tlsConf *tls.Config, c *cache, blockLi
 					} else {
 						//delete entry if it is not valid anymore
 						c.entries = append((c.entries)[:i], (c.entries)[i+1:]...)
-
 					}
 				}
 			}
 			c.Unlock()
 
-			//check blocklist for the question
-			if m.Question[0].Qtype == dns.TypeA {
-				if ip, ok := blockList[m.Question[0].Name]; ok {
-					dontQuery = true
-					log.Println("blocked:", m.Question[0].Name, "to", ip)
-					response := new(dns.Msg)
-					response.SetReply(m)
-					response.Answer = append(response.Answer, &dns.A{
-						Hdr: dns.RR_Header{
-							Name:   m.Question[0].Name,
-							Rrtype: dns.TypeA,
-							Class:  dns.ClassINET,
-							Ttl:    0,
-						},
-						A: net.ParseIP(ip),
-					})
+			if block {
+				//check blocklist for the question
+				if m.Question[0].Qtype == dns.TypeA {
+					if ip, ok := blockList[m.Question[0].Name]; ok {
+						dontQuery = true
+						log.Println("blocked:", m.Question[0].Name, "to", ip)
+						response := new(dns.Msg)
+						response.SetReply(m)
+						response.Answer = append(response.Answer, &dns.A{
+							Hdr: dns.RR_Header{
+								Name:   m.Question[0].Name,
+								Rrtype: dns.TypeA,
+								Class:  dns.ClassINET,
+								Ttl:    0,
+							},
+							A: net.ParseIP(ip),
+						})
 
-					responseRaw, err := response.Pack()
-					if err != nil {
-						return
-					}
+						responseRaw, err := response.Pack()
+						if err != nil {
+							return
+						}
 
-					//add 2 bytes length to the beginning of the packet
-					responseRaw = append([]byte{byte(len(responseRaw) >> 8), byte(len(responseRaw))}, responseRaw...)
+						//add 2 bytes length to the beginning of the packet
+						responseRaw = append([]byte{byte(len(responseRaw) >> 8), byte(len(responseRaw))}, responseRaw...)
 
-					_, err = localConn.Write(responseRaw)
-					if err != nil {
-						return
+						_, err = localConn.Write(responseRaw)
+						if err != nil {
+							return
+						}
 					}
 				}
 			}
@@ -224,9 +223,12 @@ func handleConnection(localConn net.Conn, tlsConf *tls.Config, c *cache, blockLi
 func main() {
 	upstream = flag.String("upstream", "208.67.220.220:853", "upstream DNS over TLS server to use, format is ipaddr:port")
 	localPort = flag.String("local", "5353", "local port to listen on") //todo: change to 853
+	blocklistBool := flag.Bool("block", true, "wheter or not to use a blocklist")
+	blocklistUrl := flag.String("blocklist", "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts", "url of blocklist to use")
 	flag.Parse()
 
-	blocklist := loadList()
+	blocklist := loadList(*blocklistUrl)
+
 	var c cache
 
 	certPEMBlock, err := os.ReadFile("cert.pem")
@@ -272,6 +274,6 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		go handleConnection(localConn, tlsConf, &c, blocklist)
+		go handleConnection(localConn, tlsConf, &c, blocklistBool, blocklist)
 	}
 }
